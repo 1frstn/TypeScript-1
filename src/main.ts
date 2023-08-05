3bb8a5a5cfbbb384f4dcb91dd62d25fde4aec7b2
1frstn <krmilhn@icloud.com>
2023-08-05 12:52:05 +1000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import './css/index.css'
+import FullList from './model/FullList'
+import ListItem from './model/ListItem'
+import ListTemplate from './templates/ListTempplate'
+
+
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent):void => {
+    e.preventDefault()
+
+    const input = document.getElementById("newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if(!newEntryText) return
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1
+
+    const newItem = new ListItem(itemId.toString(), newEntryText)
+
+    fullList.addItem(newItem)
+
+    template.render(fullList)
+
+  })
+
+  const clearItem = document.getElementById("clearItemButton") as HTMLButtonElement
+
+  clearItem.addEventListener('click', (): void =>{
+    fullList.clearList()
+    template.clear()
+  } )
+
+  fullList.load()
+  template.render(fullList)
+}
+
+document.addEventListener("DOMContentLoaded",initApp)
